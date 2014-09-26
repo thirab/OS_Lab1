2bbe51555450643df0e1e74dee58cb364a72e805
thirab <thirabayashi@hitpointstudios.com>
2014-09-26 01:45:36 -0400

    Created FCFS scheduler
    Also works as a main for reading in files etc( depends on how we want
    to use them). Operated under the assumption that job ID’s are
    sequential. Otherwise I can change this (the example had sequential
    jobs, 1,2,3,)

---
+++ Scheduler.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Scheduler {
+	
+	public static void fcfsScheduler(String fileName) throws IOException{
+		BufferedReader fileLines = new BufferedReader(new FileReader(fileName));
+		String line;
+		int totalTime =0;
+		int processes =0;
+		int waitTime=0;
+		while ((line = fileLines.readLine()) != null) {
+			
+			String[] splitLine = line.split(",");
+			//job id in this case is irrelevant as information inputted is sequential
+			int jobId = Integer.parseInt(splitLine[0]);
+			int seconds = Integer.parseInt(splitLine[1]);
+			
+			//this is a new process
+			processes++;
+			//the wait time is the time all inputs ahead of this process have taken
+			waitTime+=totalTime;
+			//the total time is totaltime + the runtime of this process
+			totalTime+=seconds;
+		   // process the line.
+		}
+		System.out.println("For fcfs scheduler: ");
+		System.out.println("The Turnaround is" + totalTime);
+		System.out.println("The average time is" + totalTime/processes);
+		System.out.println("The throughput is: " + processes/(totalTime/60) );
+		System.out.println("The wait time is: " + waitTime);
+		System.out.println("The Average wait time is: " + waitTime/processes);
+		System.out.println("------");
+		fileLines.close();
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws IOException {
+
+			BufferedReader br = 
+					new BufferedReader(new InputStreamReader(System.in));
+
+			String fileName=br.readLine() ;
+			br.close();
+			fcfsScheduler(fileName);
+			//TODO the other schedulers
+
+	}
+
+
+}
